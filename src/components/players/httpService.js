c3a07b70d9042dc86d2f9b5ddfe6bda56c37337c
redlukas <redlukas@users.noreply.github.com>
2021-08-15 17:25:11 +0200

    better error display

--- src/components/players/httpService.js
+++ src/components/players/httpService.js
@@ -5,7 +5,7 @@ import { toast } from "react-toastify";
 axios.interceptors.response.use(null, error => {
   const expectedError =
     error.response &&
-    error.response.status > 400 &&
+    error.response.status >= 400 &&
     error.response.status < 500;
 
   if (!expectedError) {
