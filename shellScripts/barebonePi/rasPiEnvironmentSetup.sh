3136ff8c19bd8dfa72832910bd962afffd332752
redlukas <redlukas@users.noreply.github.com>
2021-08-24 11:34:06 +0200

    mvp of install script?

--- shellScripts/barebonePi/rasPiEnvironmentSetup.sh
+++ shellScripts/barebonePi/rasPiEnvironmentSetup.sh
@@ -15,8 +15,8 @@ systemctl start mongodb
 
 ##pull the repos
 cd /home/pi/
-git pull https://github.com/redlukas/winingsfrontend.git
-git pull https://github.com/redlukas/winningscalculator.git
+git clone https://github.com/redlukas/winingsfrontend.git
+git clone https://github.com/redlukas/winningscalculator.git
 
 ##install the npm deps
 cd winningscalculator
@@ -25,8 +25,16 @@ cd ../winingsfrontend
 npm i
 
 ##make the scripts executable
+chmod +x /home/pi/winningscalculator/back.sh
+chmod +x /home/pi/winingsfrontend/shellScripts/front.sh
+chmod +x /home/pi/winingsfrontend/shellScripts/resetconfigfile.sh
+chmod +x /home/pi/winingsfrontend/shellScripts/gitpull.sh
 
 ##register the startup script with cron
-
+crontab -l > crontemp
+echo "@reboot /bin/bash /home/pi/winningscalculator/back.sh" >> crontemp
+echo "@reboot /bin/bash /home/pi/winingsfrontend/shellScripts/front.sh" >> crontemp
+crontab crontemp
+rm crontemp
 
 
